a981e9cf131b47cc0fe247b07310d39ce6c04ba5
caoshengyong <cao.shengyong06199@westone.com.cn>
2020-06-09 19:10:35 +0800

    spring boot admin server

--- adminClientDemoEureka/src/main/java/client/AdminClientEurekaApplication.java
+++ adminClientDemoEureka/src/main/java/client/AdminClientEurekaApplication.java
@@ -1,4 +1,4 @@
-package provider;
+package client;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,8 +7,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class ProviderEurekaApplication {
+public class AdminClientEurekaApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ProviderEurekaApplication.class,args);
+        SpringApplication.run(AdminClientEurekaApplication.class,args);
     }
 }
---
+++ simpleSpringBootSecurity/src/main/java/com/test/springboot/SimpleSecurityApplication.java
@@ -0,0 +1,14 @@
+package com.test.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class SimpleSecurityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSecurityApplication.class,args);
+    }
+}
